9eba562d485f981f3e60a0982140d012c2230a50
Lucas Klemmer <klemmer@uni-bremen.de>
2022-02-22 22:10:05 +0100

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,3 +1,4 @@
+#!/bin/python
 from wal.core import Wal
 import sys
 import os
@@ -7,7 +8,17 @@ def ranges(binary_file):
     '''This functions extracts the functions from elf files using "nm"'''
     res = []
 
-    proc = subprocess.Popen( [ os.environ['RISCV_PREFIX'] + 'nm' , '-S', binary_file], stdout=subprocess.PIPE )
+    if 'RISCV_PREFIX' in os.environ:
+        proc = subprocess.Popen( [ os.environ['RISCV_PREFIX'] + 'nm' , '-S', binary_file], stdout=subprocess.PIPE )
+    else:
+        print('Please add the location of you RISC-V toolchain to the "RISCV_PREFIX" variable.')
+        print('Now trying to fall back to the regular "nm"..\n')
+        try:
+            proc = subprocess.Popen( [ 'nm' , '-S', binary_file], stdout=subprocess.PIPE )
+        except:
+            print('"nm" not found on system')
+            exit(1)
+        
     stdout, _ = proc.communicate()
     symbols = stdout.decode('UTF-8')
 
@@ -31,10 +42,6 @@ if __name__ == '__main__':
         print('  profile.py elf vcd')
         exit(1)
 
-    if 'RISCV_PREFIX' not in os.environ:
-        print('Please add RISCV_PREFIX to you path')
-        exit(1)
-
     BIN = sys.argv[1]
     VCD = sys.argv[2]
 
@@ -42,19 +49,23 @@ if __name__ == '__main__':
 
     wal = Wal()
     wal.load(VCD)
-    wal.eval('(require config)') # require config script to get concrete signal names        
+    wal.eval('(require config)') # require config script to get concrete signal names
     wal.eval('''(defun count-function [addr]
                   (for [f funcs]
                     (when (&& (>= addr f[1]) (<= addr f[2]))
-    	              (seta dist f[0] (+ (geta/default dist 0 f[0]) 1)))))''')
+    	                (seta dist f[0] (+ (geta dist f[0]) 1)))))''')
 
     # calculate the time spent in each function, does not take subcalls into account
     dist = {}
-    wal.eval('(whenever (&& clk fire) (count-function pc))', funcs=functions, dist=dist)
+    wal.eval('(whenever (&& (> INDEX 0) clk fire) (count-function pc))', funcs=functions, dist=dist)
     # get number of executed instructions
-    instructions_executed = wal.eval('(length (find (&& clk fire)))')
+    instructions_executed = wal.eval('(length (find (&& (> INDEX 0) clk fire)))')
 
     # print results
+    in_known_function = 0
     print('Time spent in functions:')
     for name, count in dist.items():
-        print(f'{name:>10}: {count/instructions_executed*100:.2f}% ({count})')
+        in_known_function += count
+        print(f'{name:>20}: {count/instructions_executed*100:.2f}% ({count})')
+
+    print(f'{"other":>20}: {(instructions_executed - in_known_function)/instructions_executed*100:.2f}% ({instructions_executed - in_known_function})')
